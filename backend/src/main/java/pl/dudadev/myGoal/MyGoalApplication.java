e62b37ee4c503d6ec2dcd9c52f9e29d28540dc34
Piotr Duda <piotrduda1992@gmail.com>
2017-10-07 17:38:59 +0200

    First commit

---
+++ backend/src/main/java/pl/dudadev/myGoal/MyGoalApplication.java
@@ -0,0 +1,12 @@
+package pl.dudadev.myGoal.myGoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGoalApplication.class, args);
+	}
+}
